a75b1ff981ab92281522761642b1cfa1a4c2b889
wendy <wendy@infopro.tw>
2024-11-26 14:47:35 +0000

    default plant backend changes

--- Assets/Scripts/DefaultPlantBackend.cs
+++ Assets/Scripts/DefaultPlantBackend.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Random = UnityEngine.Random;
 
 public class PlantData
 {
     public int Stage = 1;
-    public HashSet<char> CompletedSyllabus = new HashSet<char>();
+    public Dictionary<char, SyllabusData> Syllabus = new Dictionary<char, SyllabusData>();
+    public bool PendingStageIncrease = false;
+}
+
+public class SyllabusData
+{
+    public int Streak = 0;
+    public int CorrectAnswers = 0;
 }
 
 public class DefaultPlantBackend : IPlantBackend
@@ -43,23 +51,32 @@ public class DefaultPlantBackend : IPlantBackend
         {
             if (_plantDatas[GetPlantIndex(question.plant)].Stage != question.stage) continue;
 
-            if (_plantDatas[GetPlantIndex(question.plant)].CompletedSyllabus
-                .Contains(question.syllabusReference)) continue;
+            Dictionary<char, SyllabusData> syllabus = _plantDatas[GetPlantIndex(question.plant)].Syllabus;
+            if (syllabus.ContainsKey(question.syllabusReference) &&
+                (syllabus[question.syllabusReference].Streak >= 2 ||
+                 syllabus[question.syllabusReference].CorrectAnswers >= 3)) continue;
 
             // add to list
             questions.Add(question);
         }
 
         // if the list has no question for a particular plant, increase the stage of plant
-        if (questions.Count == 0)
+        foreach (var plantEntry in _plantLibrary.plantEntries)
         {
-            HandleIncreasePlantLevel();
+            if (questions.Any(question => question.plant == plantEntry))
+            {
+                HandleIncreasePlantLevel(plantEntry);
+            }
+                
         }
 
         // random choose a question from list
         QuestionEntry randomQuestion = questions[Random.Range(0, questions.Count)];
         QuestionSet questionSet = new();
-        questionSet.DisplayQuestion = FillQuestionString(randomQuestion);
+
+        PlantData plantData = _plantDatas[GetPlantIndex(randomQuestion.plant)];
+        bool includeHint = plantData.Syllabus.ContainsKey(randomQuestion.syllabusReference);
+        questionSet.DisplayQuestion = FillQuestionString(randomQuestion, includeHint);
 
         // get possible answers (right & wrong)
         List<Answer> possibleAnswers = new List<Answer>();
@@ -74,6 +91,8 @@ public class DefaultPlantBackend : IPlantBackend
 
         _createdQuestionSets.Add(questionSet);
         _createdQuestionEntries.Add(randomQuestion);
+        _plantDatas[GetPlantIndex(randomQuestion.plant)].Syllabus
+            .Add(randomQuestion.syllabusReference, new SyllabusData());
         return questionSet;
     }
 
@@ -115,11 +134,11 @@ public class DefaultPlantBackend : IPlantBackend
         return wrongAnswers;
     }
 
-    private string FillQuestionString(QuestionEntry question)
+    private string FillQuestionString(QuestionEntry question, bool includeHint)
     {
         // Use [H] for hint, [P] for plant, [O] for feature/object with the square brackets.
         var baseQuestion = question.questionType.question;
-        baseQuestion = baseQuestion.Replace("[H]", question.hint);
+        baseQuestion = baseQuestion.Replace(" [H]", includeHint ? " " + question.hint : "");
         baseQuestion = baseQuestion.Replace("[P]", question.plant.displayName);
         string feature;
         if (question.questionType.useFeaturePluralName)
@@ -140,9 +159,15 @@ public class DefaultPlantBackend : IPlantBackend
         return baseQuestion;
     }
 
-    private void HandleIncreasePlantLevel()
+    private void HandleIncreasePlantLevel(PlantEntryScriptableObject plantEntry)
     {
-        throw new System.NotImplementedException();
+        // Reset & add pending level to increase
+        _plantDatas[GetPlantIndex(plantEntry)].PendingStageIncrease = true;
+        foreach (var syllabus in _plantDatas[GetPlantIndex(plantEntry)].Syllabus)
+        {
+            syllabus.Value.CorrectAnswers = 0;
+            syllabus.Value.Streak = 0;
+        }
     }
 
     public bool AttemptQuestion(AnswerHandle handle)
@@ -155,17 +180,23 @@ public class DefaultPlantBackend : IPlantBackend
                 if (handle.Key != possibleAnswer.Handle.Key) continue;
                 if (possibleAnswer.IsCorrect)
                 {
-                    // if correct add complete syllabus
-                    _plantDatas[GetPlantIndex(_createdQuestionEntries[i].plant)].CompletedSyllabus
-                        .Add(_createdQuestionEntries[i].syllabusReference);
+                    // if correct add syllabus streak+1 and correctAnswer+1
+                    _plantDatas[GetPlantIndex(_createdQuestionEntries[i].plant)].Syllabus
+                        [_createdQuestionEntries[i].syllabusReference].Streak += 1;
+                    _plantDatas[GetPlantIndex(_createdQuestionEntries[i].plant)].Syllabus
+                        [_createdQuestionEntries[i].syllabusReference].CorrectAnswers += 1;
                     return true;
                 }
+
+                _plantDatas[GetPlantIndex(_createdQuestionEntries[i].plant)].Syllabus
+                    [_createdQuestionEntries[i].syllabusReference].Streak = 0;
                 return false;
             }
         }
+
         throw new Exception("Could not find answer handle");
     }
-    
+
     // TODO: Clear the cache when changing scenes
     public void ClearQuestionCache()
     {
@@ -176,5 +207,28 @@ public class DefaultPlantBackend : IPlantBackend
     public int GetStage(PlantEntryScriptableObject plant)
     {
         return _plantDatas[GetPlantIndex(plant)].Stage;
+    }
+
+    public List<StageIncrease> EndSession()
+    {
+        List<StageIncrease> stageIncreases = new List<StageIncrease>();
+        for (int i=0; i<_plantDatas.Count; i++)
+        {
+            PlantData plantData = _plantDatas[i];
+            if (plantData.PendingStageIncrease)
+            {
+                plantData.Stage ++;
+                plantData.PendingStageIncrease = false;
+                plantData.Syllabus.Clear();
+                
+                StageIncrease stageIncrease = new StageIncrease
+                {
+                    Plant = _plantLibrary.plantEntries[i],
+                    NewStage = plantData.Stage
+                };
+                stageIncreases.Add(stageIncrease);
+            }
+        }
+        return stageIncreases;
     }
 }
