93a67f08d39a7637517fd99849b414bb2eca2e13
Trev <58667461+TheTrueTree@users.noreply.github.com>
2024-11-27 12:30:41 +0000

    Fix persistance

--- Assets/Scripts/Persistance.cs
+++ Assets/Scripts/Persistance.cs
@@ -3,14 +3,22 @@ using UnityEngine;
 public class Persistance : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-
-    public PlantLibraryScriptableObject plantLibrary;
-    public QuestionLibraryScriptableObject questionLibrary;
-    public DefaultPlantBackend backend;
-    void Start()
+    
+    public static Persistance instance { get; private set; }
+    
+    public static PlantLibraryScriptableObject plantLibrary;
+    public static QuestionLibraryScriptableObject questionLibrary;
+    public static DefaultPlantBackend backend;
+    void Awake()
     {
+        DontDestroyOnLoad(this);
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        
+        instance = this;
         backend = new DefaultPlantBackend(plantLibrary, questionLibrary);
-        DontDestroyOnLoad(this.gameObject);
     }
     
 }
